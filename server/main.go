a009153ec72f96214d864f773fa6fc1abadea5ee
jiangtengfei2261 <jiangtengfei2261@ipalfish.com>
2020-09-14 20:33:49 +0800

    register service

--- server/main.go
+++ server/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/coreos/etcd/clientv3"
 	pb "github.com/jiangtengfei/go-docker-tutorial-pub/grpc"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"time"
 )
 
 const port  = ":9090"
 
 type server struct {
-	pb.UnimplementedGreeterServer
 }
 
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
@@ -24,9 +26,32 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
+	registService()
+
 	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+func registService() {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		log.Print("error occurred while create etcd client")
+		return
+	}
+	defer cli.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	resp, err := cli.Put(ctx, "server_ip", "127.0.0.1:9090")
+	cancel()
+	if err != nil {
+		fmt.Errorf("error while put: %+v", err)
+	}
+	log.Printf("resp: %+v", resp)
 }
